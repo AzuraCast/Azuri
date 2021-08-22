c39d7d2be623bdcceb2a0d8a569e25479b6eab10
TwixGamer00 <64733980+TwixGamer00@users.noreply.github.com>
2021-08-22 12:32:50 +0200

    Update app.mjs

--- src/app.mjs
+++ src/app.mjs
@@ -4,13 +4,13 @@ import fs from 'fs';
 
 dotenv.config();
 
-if (!fs.existsSync('./persist/guilds.json')) {
+/*if (!fs.existsSync('./persist/guilds.json')) {
     fs.copyFile('./persist/guilds.json.example', './persist/guilds.json', (err) => { 
         if (err) { 
           console.log("Error Found:", err); 
         }
     });
-}
+}*/
 
 const manager = new ShardingManager('./index.mjs', { token: process.env.BOT_TOKEN });
 manager.on('shardCreate', shard => console.log(`Starting Shard: ${shard.id}`));
