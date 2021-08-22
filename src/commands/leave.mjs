913029b5f22d476b7ff5d188509557d311f146a6
TwixGamer00 <ren789.nl@gmail.com>
2021-08-22 16:25:00 +0200

    Added option to change permissions for leave command (.env)

--- src/commands/leave.mjs
+++ src/commands/leave.mjs
@@ -7,6 +7,15 @@ export default {
   execute: async (client, guildData, message, ...args) => {
     if (!message.guild)
       return message.channel.send(L._U(guildData.locale, "server_only"));
+    if (!process.env.EVERYONE_LEAVE) {
+      if (
+        !message.channel.permissionsFor(message.member).has("MANAGE_GUILD") ||
+        !message.channel.permissionsFor(message.member).has("ADMINISTRATOR")
+      )
+        return message.channel.send(
+          "❌ - Oh No! You've not got permission to use that!"
+        );
+    }
     if (!(await dv.getVoiceConnection(message.guild.id)))
       return message.channel.send(L._U(guildData.locale, "not_connected"));
 
